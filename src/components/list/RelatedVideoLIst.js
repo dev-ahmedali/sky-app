525e7548cef68b1ba3cc054f1373ddc5d274172d
dev-ahmedali <ahmedali69325@gmail.com>
2022-11-01 12:30:33 +0600

    slice api & ui intregration

--- src/components/list/RelatedVideoLIst.js
+++ src/components/list/RelatedVideoLIst.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import RelatedVideoListItem from './RelatedVideoListItem'
 
-export default function RelatedVideoLIst() {
+export default function RelatedVideoLIst({currentVideoId, tags}) {
   return (
     <div>
         <div class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
--- src/features/videos/VideosSlice.js
+++ src/features/videos/VideosSlice.js
@@ -14,7 +14,7 @@ export const fetchVideos = createAsyncThunk("videos/fetchVideos", async () => {
   return videos;
 });
 
-const videoSlice = createSlice({
+const videosSlice = createSlice({
   name: "videos",
   initialState,
   extraReducers: (builder) => {
@@ -36,4 +36,4 @@ const videoSlice = createSlice({
   },
 });
 
-export default videoSlice.reducer;
+export default videosSlice.reducer;
