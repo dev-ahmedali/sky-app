bdcee639abbd1279b00f9a68bc34227b1aecadda
dev-ahmedali <ahmedali69325@gmail.com>
2022-11-01 13:38:47 +0600

    added related videos query

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import tagsReducer from '../features/tags/tagsSlice';
 import videosReducer from '../features/videos/VideosSlice';
 import videoReducer from '../features/video/videoSlice';
+import relatedVideosReducer from '../features/relatedVideos/relatedVideosSlice';
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     tags: tagsReducer,
     video: videoReducer,
+    relatedVideos: relatedVideosReducer,
   },
 });
--- src/components/list/RelatedVideoLIst.js
+++ src/components/list/RelatedVideoLIst.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchRelatedVideos } from '../../features/relatedVideos/relatedVideosSlice'
+import Loading from '../ui/Loading'
 import RelatedVideoListItem from './RelatedVideoListItem'
 
 export default function RelatedVideoLIst({currentVideoId, tags}) {
+  const {relatedVideos, isLoading, isError, error} = useSelector((state) => state.relatedVideos)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchRelatedVideos({tags, id: currentVideoId}))
+  }, [dispatch, tags, currentVideoId])
+
+  // decide what to render
+  let content = null
+
+  if (isLoading) content = <Loading/>
+
+  if (!isLoading && isError) content = <div className="col-span-12">{error}</div>;
+
+  if (!isLoading && !isError && relatedVideos?.length === 0) {
+    content = <div className="col-span-12">No related videos found!</div>;
+  }
+
+  if (!isLoading && !isError && relatedVideos?.length > 0) {
+    content = relatedVideos.map((video) => <RelatedVideoListItem key={video.id} video={video}/>) 
+  }
+  
   return (
     <div>
         <div class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
-           <RelatedVideoListItem/>
+           {content}
            
           </div>
     </div>
